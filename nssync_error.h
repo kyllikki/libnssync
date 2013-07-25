9c949717bd45f0c35fcef3faa29dcc56102ec676
Vincent Sanders <vincent.sanders@collabora.co.uk>
2013-07-25 18:05:26 +0100

    fix enum name

--- nssync_error.h
+++ nssync_error.h
@@ -2,7 +2,7 @@
 #ifndef NSSYNC_ERROR_H
 #define NSSYNC_ERROR_H
 
-enum nnsync_error {
+enum nssync_error {
 	NSSYNC_ERROR_OK = 0, /* operationhad no error */
 	NSSYNC_ERROR_NOMEM, /* operation failed on memory allocation */
 	NSSYNC_ERROR_AUTH, /* authorisation faliure */
